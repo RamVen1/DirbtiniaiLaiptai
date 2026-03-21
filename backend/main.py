8dc3357a7178449250d75b49438e8cbc48375237
Quiilo <z.venclova14@gmail.com>
2026-03-21 19:25:45 +0200

    Created the basic file structure for the backend and moved the main code into the appropriate files. Also added bcrypt for password hashing and dotenv for environment variable management.

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,10 @@ from core.database import init_db
 from core.config import settings
 from auth.router import router as auth_router
 from tasks.router import router as tasks_router
-from jose import JWTError, jwt
-
-#Kolkas login verification su JWT info cia
-SECRET_KEY = "72j0rJhL2IUeedeP4B5UX0Z04N7FuhC1WygKwwLfIEJ"
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
+# Setup
 init_db()
 
 app.add_middleware(
@@ -25,26 +22,9 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(tasks_router)
 
-def create_access_token(data: dict):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(days=7) # Tokenas kolkas 7 dienom galioja
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
-
-def get_current_user(authorization: str = Header(None)):
-    if not authorization or not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Missing or invalid token")
-    
-    token = authorization.split(" ")[1]
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        user_id: str = payload.get("sub")
-        if user_id is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
-        return user_id
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Token expired or invalid")
 @app.get("/")
+def read_root():
+    return {"status": "online"}
 def read_root():
     return {"status": "online"}
 
